798eb69274802f6b07c38accd6b30575be90a11a
Wirt <wirt.salthouse@gmail.com>
2020-12-17 12:36:33 -0800

    want to get this to a user-testable stage

--- main.rb
+++ main.rb
@@ -1,10 +1,41 @@
 load 'suggestion_box.rb'
 load 'categories.rb'
-box = SuggestionBox. new
-box.load_box("myLovelyHorse.json")
-puts box.addSuggestion("Occupation", "Muppet")
-puts box.getSuggestion("Occupation")
-puts box.getSuggestion("Occupation")
-puts box.getSuggestion("Occupation")
-puts box.getSuggestion("Occupation")
-box.save_box("myLovelyHorse.json")
+box = SuggestionBox. new "my_suggestions"
+#a little test code to make sure everything's dry-firing correctly
+box.run_defaults
+flag = true
+output_msg= "add suggestion [A]\n pick suggestion [P]\n view categories [V] \n add categories[C]\n Quit [Q]"
+
+
+def getOption(puts_msg)
+  puts puts_msg
+  raw = gets
+  ch = raw[0]
+  return ch.upcase
+end
+
+
+#main program logic
+while flag
+  option = getOption(output_msg)
+  case option
+  when 'A'
+    #do stuff
+    puts "you hit A, wow!"
+  when 'P'
+    #do stuff
+    puts "you entered P"
+  when 'V'
+      #do stuff
+      puts "you entered V"
+  when 'C'
+    #do stuff
+    puts "you entrd C"
+  when 'Q'
+    box.save
+    puts "bye"
+    flag = false
+  else
+    puts "that was not a valid entry"
+  end
+end
--- suggestion_box.rb
+++ suggestion_box.rb
@@ -1,15 +1,47 @@
 class SuggestionBox
 #not yet seperated into categoires, will want to move to some kind of JSON storage asap
-  def initialize
+  def initialize( filename )
+    @filepath = filename+".json"
+    puts "file name is: "+@filepath
+    #try to read that filepath
+    #if filepath doesn't exist, create it and call "run defaults"
     @locations = ["Laundromat", "Starbucks", "The Woods", "Paris"]
     @occupations = ["Doctor", "Plumber", "Wizard", "Cobbler", "Theif"]
     @genres = ["Bollywood", "Western", "Opera", "Film Noir", "Shakespeare"]
     @relationships  = ["Divorced Parents", "Coworkers", "Roommates", "Hookups", "King/Serf"]
   end
 
-  def getSuggestion(category)
-    if @
+def add_category(category)
+  #check if category is a key in the hash table
+  #return false if it's already in there
+  #else add that key with an empty array
+  #return true
 end
 
-#will want a contstructor that generates from stored JSON
-#will want to implement Suggestion Objects and encapsulate other functionality
+def add_suggestion(category, suggestion)
+end
+
+def run_defaults()
+  loc ="Location"
+  occ = "Occupation"
+  rel = "Relatinship"
+  add_category(loc)
+  add_suggestion(loc, "laundromat")
+  add_suggestion(loc, "the gym")
+  add_suggestion(loc, "an office")
+  add_category(occ)
+  add_suggestion(occ, "dentist")
+  add_suggestion(occ, "theif")
+  add_suggestion(occ, "plumber")
+  add_category(rel)
+  add_suggestion(rel, "best friends")
+  add_suggestion(rel, "coworker")
+  add_suggestion(rel, "grandparents")
+end
+
+def save
+  #write the current hashtable to json
+  #save json in the filepath determined in the constructor
+end
+
+end
